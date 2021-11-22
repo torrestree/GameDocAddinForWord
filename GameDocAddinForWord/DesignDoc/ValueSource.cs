0582d687f47e150d892d883a113cef7255531ba7
torrestree <drizzt.tree@hotmail.com>
2021-11-22 10:19:45 +0800

    2021 11 22 ValueSource

---
+++ GameDocAddinForWord/DesignDoc/ValueSource.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDocAddinForWord.DesignDoc
+{
+    internal class ValueSource
+    {
+    }
+}
